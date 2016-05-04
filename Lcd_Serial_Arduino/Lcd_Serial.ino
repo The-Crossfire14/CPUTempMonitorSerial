6b21825e64569e7be27660ebe3f5c619aec3e8c5
The-Crossfire14 <jimmyullmaniii@gmail.com>
2016-05-04 09:31:21 -0400

    Added files via upload

---
+++ Lcd_Serial_Arduino/Lcd_Serial.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // when characters arrive over the serial port...
+  if (Serial.available()) {
+    // wait a bit for the entire message to arrive
+    delay(500);
+    // clear the screen
+    lcd.clear();
+    // read all the available characters
+    while (Serial.available() > 0) {
+      // display each character to the LCD
+      lcd.write(Serial.read());
+    }
+  }
+}
